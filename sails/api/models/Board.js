f1ddf90ddf2494518d1ef1b7062939cca703813a
sd25 <simon.deuring@uni-ulm.de>
2019-12-19 00:09:28 +0100

    Improve backend config

--- sails/api/models/Board.js
+++ sails/api/models/Board.js
@@ -1,8 +1,19 @@
 module.exports = {
+  primaryKey: 'id',
   attributes: {
+<<<<<<< HEAD
     id: { type: 'number', autoIncrement: true },
     createdAt: { type: 'number', autoCreatedAt: true },
     updatedAt: { type: 'number', autoUpdatedAt: true },
     boardname: {type: 'string', required: true, unique: true},
+=======
+    id: { type: 'number', required: true, unique: true, columnName: 'board_id' },
+    boardName: { type: 'string', required: true, unique: true, columnName: 'board_name' },
+    department: { type: 'number', allowNull: true },
+    createdAt: { type: 'number', columnName: 'created_at' },
+    updatedAt: { type: 'number', columnName: 'updated_at' }
+>>>>>>> f591cb5028aa71366ba52f6d5340a93c1c6d0f97
   },
+  createdAt: 'createdAt',
+  updatedAt: 'updatedAt'
 };
--- sails/api/models/Post.js
+++ sails/api/models/Post.js
@@ -1,15 +1,24 @@
 module.exports = {
+  primaryKey: 'id',
   attributes: {
-    id: { type: 'number', unique: true, autoIncrement: true },
-    createdAt: { type: 'number', autoCreatedAt: true },
-    updatedAt: { type: 'number', autoUpdatedAt: true },
-    title: { type: 'string' },
+    id: { type: 'number', required: true, unique: true, columnName: 'post_id' },
+    title: { type: 'string', allowNull: true},
     typeOfPost: {
       type: 'string',
       isIn: ['note','pdf','poll','survey','calendar','image'],
-      required: true
+      required: true,
+      columnName: 'type_of_post'
     },
+<<<<<<< HEAD
     board: { type: 'number', required: true },
     content: {type: 'string'},
+=======
+    content: { type: 'json' }, // could carry a link to a file
+    boardId: { type: 'number', required: true, columnName: 'board_id' },
+    createdAt: { type: 'number', columnName: 'created_at' },
+    updatedAt: { type: 'number', columnName: 'updated_at' }
+>>>>>>> f591cb5028aa71366ba52f6d5340a93c1c6d0f97
   },
+  createdAt: 'createdAt',
+  updatedAt: 'updatedAt'
 };
