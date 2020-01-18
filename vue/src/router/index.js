494e220067ef0ff0125eda299bab470b7cf99e2d
Sebastian Lehmann <Sebastian.Lehmann@Uni-Ulm.de>
2020-01-18 17:24:53 +0100

    Added support for i18n

--- vue/src/router/index.js
+++ vue/src/router/index.js
@@ -1,21 +1,48 @@
 import Vue from 'vue';
-import VueRouter from 'vue-router';
-import Home from '../views/Home.vue';
-Vue.use(VueRouter);
+import Router from 'vue-router';
 
-const routes = [{
-  path: '/',
-  name: 'noticeBoard',
-  // route level code-splitting
-  // this generates a separate chunk (about.[hash].js) for this route
-  // which is lazy-loaded when the route is visited.
-  component: () =>
-    import ('../views/NoticeBoard.vue')
-}
-];
+import RouterView from '@/views/RouterView.vue';
+import { i18n } from '@/main.js';
 
-const router = new VueRouter({
-  routes
+Vue.use(Router);
+
+export default new Router({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes: [{
+    //check for language prefix (i.e /en/ or /de/). Default to /en/ if no valid prefix was found
+    path: '/:lang/',
+    component: RouterView, //empty router object. Do not delete.
+    beforeEnter(to,from,next){
+      const lang = to.params.lang;
+      //redirect to empty /en page
+      //TODO replace '/en' with usefull language info or 404 page
+      if (!['en', 'de'].includes(lang)) { return next('/en'); }
+      if (i18n.locale !== lang) {
+        i18n.locale = lang;
+      }
+      return next();
+    },
+
+    children: [
+      {
+        path: 'about',
+        name: 'About',
+        component: () => import ('../views/About.vue')
+      },
+      {
+        path: 'home',
+        name: 'Home',
+        component: () => import ('../views/NoticeBoard.vue')
+      }
+    ]
+
+  },
+  //Redirect to 404 page if trying to visit invalid path
+  {
+    path: '*',
+    //redirect: '/en/home'
+    component: () => import ('../views/404.vue')
+  }
+  ]
 });
-
-export default router;
