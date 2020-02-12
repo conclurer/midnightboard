7f7af81edd6c39aebe667378e9906b0b694cf6f0
Simon-Deuring <simon.deuring@uni-ulm.de>
2020-02-12 11:26:10 +0100

    Fix Linter warnings

--- vue/src/main.js
+++ vue/src/main.js
@@ -4,8 +4,8 @@ import router from './router'
 import store from './store'
 
 // Import regenerator-runtime
-import "core-js/stable";
-import "regenerator-runtime/runtime";
+import 'core-js/stable'
+import 'regenerator-runtime/runtime'
 
 // Import Bootstrap
 import BootstrapVue from 'bootstrap-vue'
@@ -68,9 +68,9 @@ import { VueMasonryPlugin } from 'vue-masonry'
 Vue.use(VueMasonryPlugin)
 
 // Import VueCtkDateTimePicker
-import VueCtkDateTimePicker from 'vue-ctk-date-time-picker';
-import 'vue-ctk-date-time-picker/dist/vue-ctk-date-time-picker.css';
-Vue.component('VueCtkDateTimePicker', VueCtkDateTimePicker);
+import VueCtkDateTimePicker from 'vue-ctk-date-time-picker'
+import 'vue-ctk-date-time-picker/dist/vue-ctk-date-time-picker.css'
+Vue.component('VueCtkDateTimePicker', VueCtkDateTimePicker)
 
 Vue.config.productionTip = false
 
