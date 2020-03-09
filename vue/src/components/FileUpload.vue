df58b0ee8aae2fb515229f4413ffb0e08595bb18
Simon-Deuring <simon.deuring@uni-ulm.de>
2020-03-09 15:29:01 +0100

    Add code to display icons for office files

--- vue/src/components/FileUpload.vue
+++ vue/src/components/FileUpload.vue
@@ -132,15 +132,6 @@ export default {
 </script>
 
 <style scoped>
-  #fileUpload {
-    font-family: 'Avenir', Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
-    margin-top: 60px;
-  }
-
   .fileTitle {
     width: 90%;
     margin-right: auto;
--- vue/src/components/ImageUpload.vue
+++ vue/src/components/ImageUpload.vue
@@ -79,15 +79,6 @@ export default {
 </script>
 
 <style scoped>
-  #imageUpload {
-    font-family: 'Avenir', Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
-    margin-top: 60px;
-  }
-
   .imageTitle {
     width: 90%;
     margin-right: auto;
