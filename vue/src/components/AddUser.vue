26e4a586bee8eca913b5d895f2c704a46542063e
Simon-Deuring <simon.deuring@uni-ulm.de>
2020-03-23 17:57:10 +0100

    Add code to display user list from database

--- vue/src/components/AddUser.vue
+++ vue/src/components/AddUser.vue
@@ -12,31 +12,35 @@
     <form>
       <h2 v-html="$t('profile.addUser')"></h2>
       <br>
-      <input type="text" id="fname" name="fname" :placeholder="$t('profile.firstName')" size="36">
-      <br><br>
-      <input type="text" id="lname" name="lname" :placeholder="$t('profile.lastName')" size="36">
-      <br><br>
-      <input type="text" id="uname" name="uname" :placeholder="$t('profile.username')" size="36">
-      <br><br>
-      <input type="text" id="email" name="email" :placeholder="$t('profile.email')" size="36">
-      <br><br>
-      <div align="left">
-        <table>
-          <tr>
-            <td>Role:</td>
-            <td>
-              <input type="radio" id="user" name="role" value="user">
-              <label for="user">User</label>
-            </td>
-          </tr>
-          <tr>
-            <td></td>
-            <td>
-              <input type="radio" id="admin" name="role" value="admin">
-              <label for="admin">Admin</label>
-            </td>
-          </tr>
-        </table>
+      <div class="wrapper">
+        <div class="user-values">
+          <input type="text" class="text-field" id="fname" name="fname" :placeholder="$t('profile.firstName')" size="36">
+          <br><br>
+          <input type="text" class="text-field" id="lname" name="lname" :placeholder="$t('profile.lastName')" size="36">
+          <br><br>
+          <input type="text" class="text-field" id="uname" name="uname" :placeholder="$t('profile.username')" size="36">
+          <br><br>
+          <input type="text" class="text-field" id="email" name="email" :placeholder="$t('profile.email')" size="36">
+          <br><br>
+          <div align="left">
+            <table>
+              <tr>
+                <td>{{$t('profile.role')}}:</td>
+                <td>
+                  <input type="radio" class="radio-button" id="user" name="role" value="user">
+                  <label for="user">{{$t('profile.user')}}</label>
+                </td>
+              </tr>
+              <tr>
+                <td></td>
+                <td>
+                  <input type="radio" class="radio-button" id="admin" name="role" value="admin">
+                  <label for="admin">{{$t('profile.admin')}}</label>
+                </td>
+              </tr>
+            </table>
+          </div>
+        </div>
       </div>
       <br><br>
       <input type="submit" :value="$t('profile.affiliate')">
@@ -67,16 +71,29 @@ export default {
 </script>
 
 <style scoped>
-  table {
-    margin-left: 35px;
-  }
-
   td {
     text-align: left;
+  }
+
+  label {
+    margin-left: 5px;
   }
 
   .creationBox {
     width: 400px;
     margin: 0px auto;
+  }
+
+  .radio-button {
+    margin-left: 10px;
+  }
+
+  .wrapper {
+    display: inline-block;
+  }
+
+  .user-values {
+    display: block;
+    text-align: left;
   }
 </style>
--- vue/src/components/UserList.vue
+++ vue/src/components/UserList.vue
@@ -7,26 +7,24 @@
     >
       <tr>
         <th>{{$t('userList.id')}}</th>
-        <th>{{$t('userList.creationDate')}}</th>
-        <th>{{$t('userList.lastUpdated')}}</th>
-        <th>{{$t('userList.lastSeen')}}</th>
-        <th>{{$t('userList.username')}}</th>
         <th>{{$t('userList.firstName')}}</th>
         <th>{{$t('userList.lastName')}}</th>
+        <th>{{$t('userList.username')}}</th>
         <th>{{$t('userList.email')}}</th>
+        <th>{{$t('userList.creationDate')}}</th>
+        <th>{{$t('userList.lastSeen')}}</th>
       </tr>
       <tr
         v-for="member in members"
         :key="member.id"
       >
         <td>{{member.id}}</td>
-        <td>{{member.createdAt}}</td>
-        <td>{{member.updatedAt}}</td>
-        <td>{{member.lastSeen}}</td>
-        <td>{{member.username}}</td>
         <td>{{member.firstName}}</td>
         <td>{{member.lastName}}</td>
+        <td>{{member.userName}}</td>
         <td>{{member.email}}</td>
+        <td>{{member.createdAt}}</td>
+        <td>{{member.lastSeen}}</td>
       </tr>
     </table>
   </div>
@@ -34,7 +32,7 @@
 
 <script>
 // @ is an alias to /src
-// import axios from 'axios'
+import axios from 'axios'
 
 export default {
   name: 'UserList',
@@ -42,28 +40,38 @@ export default {
   },
   data () {
     return {
-      members: [
-        {
-          id: 0,
-          createdAt: 0,
-          updatedAt: 0,
-          lastSeen: 0,
-          username: 'Sillian',
-          firstName: 'Simon',
-          lastName: 'Deuring',
-          email: 'simon@gmail.de'
-        },
-        {
-          id: 1,
-          createdAt: 0,
-          updatedAt: 0,
-          lastSeen: 0,
-          username: 'Sillian',
-          firstName: 'Simon',
-          lastName: 'Deuring',
-          email: 'simon@gmail.de'
+      members: []
+    }
+  },
+  created () {
+    axios
+      .get('http://localhost:1337/api/users/all?skipAvatar=true', {
+        headers: {
+          'Authorization': 'Bearer ' + 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiYWRtaW4iLCJpZCI6MSwiaWF0IjoxNTg0OTc1MzYxLCJleHAiOjE2MTY1MTEzNjF9.ZS_N0zQ2lxKVlMIrkOMoWhbp1ujZSPrIiqNLH2NdUyo'
         }
-      ]
+      })
+      .then(response => { this.members = response.data.sort(compare) })
+      .catch(err => {
+        switch (err.response.status) {
+          case 400:
+            window.location = '/login'
+            break
+          case 401:
+            break
+          case 500:
+            this.$log.error(err)
+            break
+          default:
+            this.$log.error(err)
+        }
+      })
+
+    function compare (a, b) {
+      if (a.id < b.id) {
+        return -1
+      } else if (a.id > b.id) {
+        return 1
+      } else { return 0 }
     }
   }
 }
