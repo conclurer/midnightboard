f3a19a71817f8a22bbacd30497baebda6387be4b
Philipp Backes <philippbackes95@gmail.com>
2020-03-26 00:15:11 +0100

    Update Swagger to v0.0.4

--- sails/api/controllers/AppController.js
+++ sails/api/controllers/AppController.js
@@ -1,9 +1,7 @@
 module.exports = {
   serve: (req, res) => {
     const fs = require('fs');
-
     const app = sails.config.appPath + '/assets/index.html';
-    //sails.log(app);
     fs.createReadStream(app).pipe(res);
   }
 };
